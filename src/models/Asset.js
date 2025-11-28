1e37f924c89553b83760629bcdc41b129254bf60
treviss11 <trevisartagrantdy@gmail.com>
2025-11-28 17:57:09 +0700

    unique ilang

--- src/models/Asset.js
+++ src/models/Asset.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const AssetSchema = new Schema({
-    nama_barang: { type: String, required: true, unique: true },
+    nama_barang: { type: String, required: true },
     jumlah: { type: Number, required: true, min: 1 },
     harga_satuan: { type: Number, required: true, min: 0 },
     total_harga: { type: Number, required: true, min: 0 },
