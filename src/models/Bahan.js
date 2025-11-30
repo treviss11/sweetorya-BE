146da2b04139ac4fe1773cd1cb5ebcbec1ac80d1
treviss11 <trevisartagrantdy@gmail.com>
2025-11-30 10:53:24 +0700

    tambah satuan

--- src/models/Bahan.js
+++ src/models/Bahan.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const BahanSchema = new Schema({
     nama_bahan: { type: String, required: true },
     stok: { type: Number, required: true, min: 0 },
-    satuan: { type: String, required: true, enum: ['ltr', 'kg', 'gr', 'cc', 'ml', 'pack', 'biji', 'pcs', 'lembar'] },
+    satuan: { type: String, required: true, enum: ['ltr', 'kg', 'gr', 'cc', 'ml', 'pack', 'biji', 'pcs', 'lembar', 'botol', 'kotak'] },
     modal_dikeluarkan: { type: Number, default: 0 },
     tgl_beli: { type: Date, default: Date.now },
     supplier: { type: String, default: '-' }
