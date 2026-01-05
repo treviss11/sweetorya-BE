743461164964fb487d48e2aea486319b6fcb64a1
treviss11 <trevisartagrantdy@gmail.com>
2026-01-05 22:59:02 +0700

    fix be

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -5,6 +5,6 @@ const { downloadFullReport } = require('../controllers/reportController');
 const { getMonthlyProfit } = require('../controllers/reportController');
 
 router.get('/download', downloadFullReport);
-router.get('/monthly', auth, getMonthlyProfit);
+router.get('/monthly', getMonthlyProfit);
 
 module.exports = router;
