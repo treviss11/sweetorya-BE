fa7861aebe06630da9de4c6e475aae1a1fa47397
treviss11 <trevisartagrantdy@gmail.com>
2026-02-02 21:16:53 +0700

    tambah satuan

--- src/models/Bahan.js
+++ src/models/Bahan.js
@@ -5,7 +5,7 @@ const BahanSchema = new Schema({
     nama_bahan: { type: String, required: true },
     stok_awal: { type: Number, required: true, min: 0 }, 
     stok_sisa: { type: Number, required: true, min: 0 },
-    satuan: { type: String, required: true, enum: ['ltr', 'kg', 'gr', 'cc', 'ml', 'pack', 'biji', 'pcs', 'lembar', 'botol', 'kotak'] },
+    satuan: { type: String, required: true, enum: ['ltr', 'kg', 'gr', 'cc', 'ml', 'pack', 'biji', 'pcs', 'lembar', 'botol', 'kotak', 'kaleng'] },
     modal_dikeluarkan: { type: Number, default: 0 },
     tgl_beli: { type: Date, default: Date.now },
     supplier: { type: String, default: '-' }
